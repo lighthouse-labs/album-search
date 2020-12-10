f38effe74b4b2eb0c2539d455c6551fc58999859
Natalia Martian <50527828+NuttyMars@users.noreply.github.com>
2020-12-09 21:39:16 -0500

    Update LiveSearch.js

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,25 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+const axios = require('axios');
+
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const getURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    
+    axios.get(getURL)
+      .then(response => {
+        console.log(response.data.results);
+        setResults([...response.data.results]);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [term]);
 
   return (
     <Fragment>
