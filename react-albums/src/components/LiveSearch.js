f803e082f0915e31cbf6e5b370e205b7f501930f
Catherine Swain <catherinemexicoswain@gmail.com>
2021-10-29 16:20:07 -0400

    walkthrough complete

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,19 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+useEffect(() => {
+  const testURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
+  axios.get(testURL).then(response => {
+    setResults([...response.data.results])
+  });
+  },[term])
+
 
   return (
     <Fragment>
--- react-albums/src/hooks/useDebounce.js
+++ react-albums/src/hooks/useDebounce.js
@@ -10,3 +10,4 @@ export default function useDebounce(input, ms) {
 
   return debounced;
 }
+
